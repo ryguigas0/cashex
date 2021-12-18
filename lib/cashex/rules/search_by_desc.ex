4318e984251b6975fc6d579826820c6b5631c7e1
ryguigas0 <guigacasa@protonmail.ch>
2021-12-18 17:42:44 -0300

    Using wildcard for searching

--- lib/cashex/rules/search_by_desc.ex
+++ lib/cashex/rules/search_by_desc.ex
@@ -5,20 +5,18 @@ defmodule Cashex.Rules.SerchByDesc do
 
   alias Cashex.{Repo, Rule}
 
+  import Ecto.Query
+
   @doc """
   Finds rules that contain the search in their descriptions
   """
   def call(search) do
-    result =
-      Repo.all(Rule)
-      |> Enum.filter(fn rule ->
-        rule.desc |> String.downcase() |> String.contains?(search |> String.downcase())
-      end)
+    query_str = "%#{search}%"
+    # WARNING: ilike/2 IS ONLY AVAILABLE ON POSTGRESQL
+    query = from r in Rule, where: ilike(r.desc, ^query_str)
 
-    if result |> length == 0 do
-      {:error, :search_no_results}
-    else
-      {:ok, result}
-    end
+    result = Repo.all(query)
+
+    {:ok, result}
   end
 end
